2fb9013a18e9fc30f50c2b39c5df14c344bead49
Srinivas Rao-Mouw <rao@artisan.co>
2024-08-18 20:29:45 -0700

    send output from database / ensure database is editable via admin

--- diver/models.py
+++ diver/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Diver(models.Model):
     name = models.CharField(max_length=200)
     dived_at = models.DateTimeField("date published")
+
+    def __str__(self):
+        return f'Diver: {self.name}'
--- diver/views.py
+++ diver/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from diver.models import Diver
 
 def index(request):
-    return HttpResponse("Dave the diver!!.")
+
+
+    names = [d.name for d in Diver.objects.all()]
+    return HttpResponse("Dave the diver!! with names: " + ", ".join(names))
